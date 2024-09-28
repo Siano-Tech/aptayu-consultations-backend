ee9b3865bdb78a2a6b67e5802e86f739e8a436fe
Siano-Tech <122805834+Siano-Tech@users.noreply.github.com>
2024-09-28 11:21:53 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -4,11 +4,7 @@ const serviceAccount = require('./aptayu-consultations.json'); // Make sure you 
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY
-    }),
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_BUCKET, // Replace with your Firebase Realtime Database URL
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET
   });

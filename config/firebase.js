0332f60fd855297afaaa2a2d96819d879194db9b
Siano-Tech <122805834+Siano-Tech@users.noreply.github.com>
2024-09-28 08:06:13 +0530

    Update firebase.js for checking if firebase admin has already initialised

--- config/firebase.js
+++ config/firebase.js
@@ -2,11 +2,13 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./aptayu-consultations.json'); // Make sure you download and place this file in the config folder
 
 // Initialize Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_BUCKET, // Replace with your Firebase Realtime Database URL
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_BUCKET, // Replace with your Firebase Realtime Database URL
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+  });
+}
 
 const db = admin.database(); // Create a reference to the Realtime Database
 module.exports = { db, admin };

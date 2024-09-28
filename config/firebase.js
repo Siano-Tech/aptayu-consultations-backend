92a42dbe6d1adbf84bc7337689d2046315f8a581
Siano-Tech <122805834+Siano-Tech@users.noreply.github.com>
2024-09-28 11:18:50 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -5,17 +5,9 @@ const serviceAccount = require('./aptayu-consultations.json'); // Make sure you 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      "type": "service_account",
-      "project_id": "aptayu-consultations",
-      "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-      "private_key": process.env.FIREBASE_PRIVATE_KEY,
-      "client_email": "firebase-adminsdk-l4ad4@aptayu-consultations.iam.gserviceaccount.com",
-      "client_id": process.env.FIREBASE_CLIENT_ID,
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-l4ad4%40aptayu-consultations.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
     }),
     databaseURL: process.env.FIREBASE_DATABASE_BUCKET, // Replace with your Firebase Realtime Database URL
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET
